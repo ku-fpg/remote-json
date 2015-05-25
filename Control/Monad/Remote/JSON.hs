86965b9b48cc1b57759e41d079566d2feec4265c
Andy Gill <andygill@ku.edu>
2015-05-25 00:20:42 -0500

    Adding draft of weak remote monad

--- Control/Monad/Remote/JSON.hs
+++ Control/Monad/Remote/JSON.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 {-|
 Module:      Control.Monad.Remote.JSON where
@@ -14,8 +15,64 @@ Portability: GHC
 
 module Control.Monad.Remote.JSON where
 
-data RPC :: * -> * where
- Pure :: a ->                     RPC a
- Bind :: RPC a -> (a -> RPC b) -> RPC b
- Ap   :: RPC (a -> b) -> RPC a -> RPC b
+import Data.Aeson
+import Data.Aeson.Types
+import Data.Text(Text)
+import Control.Transformation
+import Control.Applicative
 
+data RPC :: * -> * where
+ Pure         :: a ->                     RPC a
+ Bind         :: RPC a -> (a -> RPC b) -> RPC b
+ Ap           :: RPC (a -> b) -> RPC a -> RPC b
+ Method       :: Text -> [Value] ->       RPC Value
+ Notification :: Text -> [Value] ->       RPC ()
+
+method :: Text -> [Value] -> RPC Value
+method = Method
+
+notification :: Text -> [Value] -> RPC ()
+notification = Notification
+
+data Session = Session
+  { sync  :: Value -> IO Value
+  , async :: Value -> IO ()
+  }
+
+--session :: (Value -> IO Value) -> Session
+--session = Session
+
+data Response 
+  = Success
+  | Failure Int Text
+
+send :: Session -> RPC a -> IO a
+send s (Pure a)   = return a
+send s (Bind f k) = send s f >>= send s . k
+send s (Ap f a)   = send s f <*> send s a
+send s (Method nm args) = do
+     let m = object [ "jsonrpc" .= (2.0 :: Double)
+     	       	    , "method" .= nm
+		    , "args" .= args
+		    , "id" .= Null 
+		    ]
+     v <- sync s m
+     let p :: Object -> Parser (Text,Value)
+         p o =  (,) <$> o .: "jsonrpc" 
+       	      	    <*> o .: "result"
+     case v of
+       Object o -> case parseMaybe p o of
+	             Just ("2.0",v) -> return v
+                     _              -> return Null
+       _ -> return Null
+send s (Notification nm args) = do
+     let m = object [ "jsonrpc" .= (2.0 :: Double)
+     	       	    , "method" .= nm
+		    , "args" .= args
+		    ]
+     async s m
+     return ()
+
+-- | '#' is a generic alias for 'send'.
+instance Transformation RPC IO Session where
+   (#) = send
