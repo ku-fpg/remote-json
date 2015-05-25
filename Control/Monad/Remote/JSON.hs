c46ea5de2936880f899150277cbfa3a84a62a02e
Andy Gill <andygill@ku.edu>
2015-05-25 14:47:56 -0500

    Improving the server

--- Control/Monad/Remote/JSON.hs
+++ Control/Monad/Remote/JSON.hs
@@ -20,6 +20,7 @@ import Data.Aeson.Types
 import Data.Text(Text)
 import Control.Transformation
 import Control.Applicative
+import qualified Data.Vector as V
 
 data RPC :: * -> * where
  Pure         :: a ->                     RPC a
@@ -39,15 +40,12 @@ data Session = Session
   , async :: Value -> IO ()
   }
 
---session :: (Value -> IO Value) -> Session
---session = Session
-
 send :: Session -> RPC a -> IO a
 send s (Pure a)   = return a
 send s (Bind f k) = send s f >>= send s . k
 send s (Ap f a)   = send s f <*> send s a
 send s (Method nm args) = do
-     let m = object [ "jsonrpc" .= (2.0 :: Double)
+     let m = object [ "jsonrpc" .= ("2.0" :: Text)
      	       	    , "method" .= nm
 		    , "args" .= args
 		    , "id" .= Null 
@@ -62,7 +60,7 @@ send s (Method nm args) = do
                      _              -> return Null
        _ -> return Null
 send s (Notification nm args) = do
-     let m = object [ "jsonrpc" .= (2.0 :: Double)
+     let m = object [ "jsonrpc" .= ("2.0" :: Text)
      	       	    , "method" .= nm
 		    , "args" .= args
 		    ]
@@ -72,3 +70,47 @@ send s (Notification nm args) = do
 -- | '#' is a generic alias for 'send'.
 instance Transformation RPC IO Session where
    (#) = send
+
+server :: [ (Text, [Value] -> IO Value) ] -> Value -> IO (Maybe Value)
+server db (Object o) = case parseMaybe p o of
+        Just ("2.0",nm,Just (Array args),theId) -> call nm (V.toList args) theId
+        _ -> return $ Just $ invalidRequest
+  where
+        -- Handles both method and notification, depending on the id value.
+        call :: Text -> [Value] -> Maybe Value -> IO (Maybe Value)
+        call nm args (Just theId) = case lookup nm db of
+               Just fn -> do v <- fn args
+                             return $ Just $ object 
+                                    [ "jsonrpc" .= ("2.0" :: Text)
+                                    , "result" .= v
+                                    , "id" .= theId
+                                    ]
+               Nothing -> return $ Just $ methodNotFound theId
+        call nm args Nothing = case lookup nm db of
+               Just fn -> do _ <- fn args
+                             return $ Nothing
+               Nothing -> return $ Nothing
+
+        p :: Object -> Parser (Text,Text,Maybe Value,Maybe Value)
+        p o =  (,,,) <$> o .:  "jsonrpc" 
+                     <*> o .:  "method"
+                     <*> o .:? "args"
+                     <*> o .:? "id"
+
+server db _  = return $ Just $ invalidRequest
+
+errorResponse :: Int -> Text -> Value -> Value
+errorResponse code msg theId = object 
+        [ "jsonrpc" .= ("2.0" :: Text)
+        , "error" .= object [ "code"  .= code
+                            , "message" .= msg
+                            ]
+        , "id" .= theId
+        ]
+
+
+invalidRequest :: Value
+invalidRequest = errorResponse (-32600) "Invalid Request" Null
+
+methodNotFound :: Value -> Value
+methodNotFound = errorResponse (-32601) "Method not found"
