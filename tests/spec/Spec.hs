eb7f08aa8e34b246709571e04eb926ce44649b70
Andy Gill <andygill@ku.edu>
2015-07-29 16:58:14 -0500

    Basic tests running (but not working yet)

--- tests/spec/Spec.hs
+++ tests/spec/Spec.hs
@@ -2,37 +2,51 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 
-module Main where
+module Spec where
 
 import           Data.Aeson
 import           Data.Aeson.Types
 import           Data.Maybe
 
 import           Data.Text(Text, append, pack, unpack)
+import qualified Data.Text.IO as TIO
 import qualified Data.Text.Lazy as LT
 import           Data.Text.Lazy.Encoding(decodeUtf8)
 
+import           Control.Monad (when)
 import           Control.Monad.Remote.JSON.Router
 import           Control.Monad.Remote.JSON.Types -- for now
 import           Data.Attoparsec.ByteString
-import           Test (parseTest)
+import           Test (readTests, Test(..))
 
-f1 :: Call a -> IO a
-f1 (Method "subtract" [Number a,Number b] _) = return $ Number (a - b)
-f1 _ = fail "f1"
+f :: Call a -> IO a
+f (Method "subtract" [Number a,Number b] _) = return $ Number (a + b)
+f (Method "sum" xs _) = return $ Number $ sum $ [ x | Number x <- xs ]
+f (Notification "update" _) = return $ ()
+f (Notification "notify_hello" _) = return $ ()
+f (Notification "notify_sum" _)   = return $ ()
+f (Method nm args _) = fail $ "missing method : " ++ show (nm,args)
+f (Notification nm args) = fail $ "missing notification : " ++ show (nm,args)
 
 -- Avoid skolem
 newtype C = C (forall a . Call a -> IO a)
 
-main = sequence_ 
-        [ do let v_req = fromJust $ decode e
+main = do
+  tests <- readTests "tests/spec/Spec.txt"
+  sequence_ 
+        [ do when (i == 1) $ do
+                putStr "#" 
+                TIO.putStrLn $ testName
              putStrLn $ ("--> " ++) $ LT.unpack $ decodeUtf8 $ encode v_req
              r <- router sequence f (Send v_req)
              case r of
-               Nothing -> putStrLn "<-- // Nothing"
-               Just v_rep -> putStrLn $ ("<-- " ++) $ LT.unpack $ decodeUtf8 $ encode v_rep
+               Nothing -> return ()
+               Just v_rep -> do
+                   putStrLn $ ("<-- " ++) $ LT.unpack $ decodeUtf8 $ encode v_rep
+             when (r /= v_expect) $ do
+                         putStrLn $ ("exp " ++) $ LT.unpack $ decodeUtf8 $ encode v_expect
              putStrLn ""
-        | (C f,e) <- [ (C f1, "{\"jsonrpc\": \"2.0\", \"method\": \"subtract\", \"params\": [42, 23], \"id\": 1}")
-                     ]
+        |  (Test testName subTests) <- tests
+        ,  (i,(v_req,v_expect)) <- [1..] `zip` subTests
         ]
 
--- tests/spec/Test.hs
+++ tests/spec/Test.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
 
-module Main where
+module Test where
 
 import           Data.Aeson
 import           Data.Aeson.Parser
@@ -11,7 +11,8 @@ import           Control.Applicative
 
 import           Data.Text(Text, append, pack, unpack)
 import qualified Data.Text.Lazy as LT
-import           Data.Text.Lazy.Encoding(decodeUtf8)
+import           Data.Text.Encoding(decodeUtf8)
+import qualified Data.ByteString as BS
 
 import           Control.Monad
 import           Control.Monad.Remote.JSON.Router
@@ -19,21 +20,28 @@ import           Control.Monad.Remote.JSON.Types -- for now
 import           Data.Attoparsec.ByteString hiding (parseTest)
 
 
-data Test = Test Value Value
+data Test = Test Text [(Value,Maybe Value)]
   deriving Show
 
 lexeme :: Parser a -> Parser a
 lexeme p = ws *> p <* ws
 
 ws :: Parser ()
-ws = many (skip (inClass " \n") <|> void (string "#")) *> return ()
+ws = many (skip (inClass " \n")) *> return ()
 
 parseTest :: Parser Test
 parseTest = do
-        lexeme (string "-->") 
-        req <- json'
-        lexeme (string "<--")
-        res <- json'
-        return $ Test req res      
+        title <- lexeme (string "#" *> some (satisfy (notInClass "\n")))
+        ts <- many $ do
+                lexeme (string "-->") 
+                req <- json'
+                res <- optional (lexeme (string "<--") *> json')
+                return $ (req,res)
+        return $ Test (decodeUtf8 (BS.pack title)) ts
 
-
+readTests :: FilePath -> IO [Test]
+readTests fileName = do
+        bs <- BS.readFile fileName
+        case parseOnly (many parseTest <* ws) bs of
+          Left msg -> error msg
+          Right v  -> return v
