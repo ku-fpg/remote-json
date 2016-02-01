8698574633de6b26e02aaf8d1008efd975aa109a
Andy Gill <andygill@ku.edu>
2016-01-31 18:37:00 -0600

    Implementing the server using scotty

--- remote-json-server/Control/Monad/Remote/JSON/Server.hs
+++ remote-json-server/Control/Monad/Remote/JSON/Server.hs
@@ -3,14 +3,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Control.Monad.Remote.JSON.Server
-        ( serverReceiveAPI
+        ( -- * Ways of building the actionable parts of a Scotty server
+          scottyReceiveAPI
+        , serverReceiveAPI
         ) where
 
-import Control.Lens ((^.))
-import Control.Monad (void)
+import Control.Monad (void, forM_)
 import Control.Monad.Remote.JSON.Types (ReceiveAPI(..))
+import           Control.Monad.IO.Class
 import Data.Aeson
+import Data.Default.Class (def)
+import Data.Text (Text)
+import Web.Scotty (scottyOpts)
+import Web.Scotty.Trans as T
+import Network.Wai.Handler.Warp (setPort)
+
 
 -- | A way of building client 'SendAPI' support, using wreq.
-serverReceiveAPI :: (forall a . ReceiveAPI a -> IO a) -> IO ()
-serverReceiveAPI f = return ()
+{-
+      serverReceiveAPI :: forall e (m :: * -> *) (t :: * -> *) a.
+                          (Foldable t, ToJSON a, MonadIO m, ScottyError e) =>
+                          (ReceiveAPI (Maybe Value) -> IO (t a)) -> ActionT e m ()
+-}
+-- | Blocking function that listens on a specific port, to a specific path.
+serverReceiveAPI :: Int -> String -> (forall a . ReceiveAPI a -> IO a) -> IO ()
+serverReceiveAPI port path f = scottyOpts opts $ post (literal path) $ scottyReceiveAPI f
+  where opts = def { verbose = 0, settings = setPort port (settings def) }
+
+-- | Build the 'ActionT' action for Scotty.
+scottyReceiveAPI :: (ScottyError e, MonadIO m)
+                 => (forall a . ReceiveAPI a -> IO a) -> ActionT e m ()
+scottyReceiveAPI f = do
+                d <- jsonData
+                r <- liftIO $ f $ Receive $ d
+                case r of
+                  Nothing -> return ()
+                  Just v -> T.json v
