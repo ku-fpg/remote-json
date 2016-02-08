a9bc35e1b53d7f2cd11ba0cb52eae442b83cbf45
Andy Gill <andygill@ku.edu>
2016-02-07 20:11:24 -0600

    Better front to back system test

--- remote-json-test/System.hs
+++ remote-json-test/System.hs
@@ -7,23 +7,42 @@ import Session
 
 import Control.Remote.Monad.JSON
 import Control.Remote.Monad.JSON.Router (transport)
+import Control.Remote.Monad.JSON.Server
+import Control.Remote.Monad.JSON.Client
 import Control.Natural
+import Control.Concurrent (forkIO,threadDelay)
+
+{-
+  [ do let port = 4000 + n + m
+       print port
+       forkIO $ serverReceiveAPI port "/test-remote-json" r
+       threadDelay (1000 * 1000)
+       return $ f $ clientSendAPI ("http://localhost:" ++ show port ++ "/test-remote-json")
+-}
 
 
-sessions :: [Session]
-sessions = 
-    [ sb (Nat $ transport (rb $$))
+sessions :: IO [Session]
+sessions = do
+  rbs <- sequence
+        [ do forkIO $ serverReceiveAPI port "/test-remote-json" (rb $$)
+             threadDelay (1000 * 1000)
+             return $ Nat $ clientSendAPI ("http://localhost:" ++ show port ++ "/test-remote-json")
+        | (rb,port) <- routerBuilders `zip` [4000..]
+        ]
+  return 
+    [ sb rb
     | sb <- sessionBuilders
-    , rb <- routerBuilders
+    , rb <- rbs
     ]
 
 main:: IO()
 main = do
+        ss <- sessions
         putStrLn "## Untyped ##"
         sequence_ [ untyped s
-                  | s <- sessions 
+                  | s <- ss
                   ]
         putStrLn "## Typed ##"
         sequence_ [ typed (DSLSession s)
-                  | s <- sessions 
+                  | s <- ss 
                   ]
